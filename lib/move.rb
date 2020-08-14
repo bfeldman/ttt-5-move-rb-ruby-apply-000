676be28b63c063bdb001c411c8dfeffabd4a87a4
Brian Feldman <brian.feldman42@gmail.com>
2020-08-14 20:42:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
 def move(board, index, character = 'X')
   board[index] = character
 end
